c325d8e9659b0477dcad57923639cec4f10b1e16
prajwal9611 <100275147+prajwal9611@users.noreply.github.com>
2022-04-04 06:05:14 +0000

    initial commit

---
+++ Q7 reverse.ps1
@@ -0,0 +1,9 @@
+﻿$number = Read-Host -Prompt "Enter a Number"
+$rnumber = 0
+while ($number -ne 0){
+$rnumber =[int]$rnumber*10
+$rnumber = [math]::floor($rnumber+$number%10)
+$number = [math]::floor($number/10)
+}
+Write-Host " Reverse Number is"
+$rnumber
---
+++ Q8 palin.ps1
@@ -0,0 +1,12 @@
+﻿$number = Read-Host -Prompt "Enter a Number"
+$rnumber = 0
+$temp = $number
+while ($number -ne 0){
+$rnumber =[int]$rnumber*10
+$rnumber = [math]::floor($rnumber+$number%10)
+$number = [math]::floor($number/10)
+}
+if($temp -eq $rnumber){
+Write-Host "Palindrome Number"
+}
+$rnumber
